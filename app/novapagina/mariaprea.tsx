bce244bc25a16a6bffabbdc26c33e58d48224c6a
SusuGostoso <suelitonmdr@gmail.com>
2023-11-11 14:15:40 -0300

    Receita 3: Exercicios feitos

--- app/novapagina/mariaprea.tsx
+++
@@ -1,5 +0,0 @@
-export default function MariaPrea(message: string) {
-    return (
-      <h2>{message}</h2>
-    )
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
-//import Image from 'next/image'
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
 
-export default function Home(){
+async function getData(pesquisa: string) {
+  let linkPesquisa = "http://www.omdbapi.com/?apikey=cf01091f&s=" + pesquisa
+
+  const res = await fetch(linkPesquisa)
+ 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+ 
+export default async function Page() {
+  const data = await getData("avengers")
 
   return (
-
-     <div>
-
-        <h2>
-
-          Viva Santana!
-
-        </h2>
-
-     </div>
-
+    <div>
+      {data.Search.map( (m: { Title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; Year: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; }) => <div>{m.Title} --- {m.Year}</div>  )}
+    </div>
   )
-
 }
-
